301acb9b94e21508bb65d5e9492a92ae9e4db954
Nicolas Bent <chocholatethunder@Nicolass-MacBook-Air.local>
2020-03-29 18:04:01 -0400

    Fixed scripts to git add data

--- scripts/ontario.sh
+++ scripts/ontario.sh
@@ -9,5 +9,7 @@ mv data/ontario/total_* data/ontario/past
 mv data/ontario/table_* data/ontario/past
 jupyter nbconvert --to notebook --inplace --execute notebooks/Graphing\ Ontario.ipynb
 git add plotly/*
+git add data/ontario/all_updates.json
+git add data/ontario/cases.jsonl
 git commit -m "Updated Ontario"
 git push origin master
--- scripts/quebec.sh
+++ scripts/quebec.sh
@@ -2,5 +2,6 @@
 
 jupyter nbconvert --to notebook --inplace --execute notebooks/Graphing\ Quebec.ipynb
 git add plotly/*
+git add data/quebec/Quebec.csv
 git commit -m "Updated Quebec"
 git push origin master
