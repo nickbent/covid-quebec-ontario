b367ca8b095e59a27637facbfaab4549ea5868ad
Nicolas Bent <chocholatethunder@Nicolass-MacBook-Air.local>
2020-03-29 18:13:30 -0400

    Removed git commands from scripts

--- scripts/ontario.sh
+++ scripts/ontario.sh
@@ -8,8 +8,4 @@ python scripts/merge_new_cases.py
 mv data/ontario/total_* data/ontario/past
 mv data/ontario/table_* data/ontario/past
 jupyter nbconvert --to notebook --inplace --execute notebooks/Graphing\ Ontario.ipynb
-git add plotly/*
-git add data/ontario/all_updates.json
-git add data/ontario/cases.jsonl
-git commit -m "Updated Ontario"
-git push origin master
+
--- scripts/quebec.sh
+++ scripts/quebec.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
 jupyter nbconvert --to notebook --inplace --execute notebooks/Graphing\ Quebec.ipynb
-git add plotly/*
-git add data/quebec/Quebec.csv
-git commit -m "Updated Quebec"
-git push origin master
+
