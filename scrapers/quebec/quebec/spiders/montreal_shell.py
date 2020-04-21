d031d90968deb30fb040079f263813fd62dcb409
Nicolas Bent <chocholatethunder@Nicolass-MacBook-Air.local>
2020-04-20 20:39:07 -0400

    Updated graphs

--- scrapers/quebec/quebec/spiders/montreal_shell.py
+++ scrapers/quebec/quebec/spiders/montreal_shell.py
@@ -13,7 +13,7 @@ def main(response):
     driver.get(response.url)
 
     driver.find_element_by_xpath('//*[@id="exampleAccordionDefault"]').click() 
-    table = driver.find_element_by_xpath('//*[@id="c37759"]/div/table[1]/tbody')
+    table = driver.find_element_by_xpath('//*[@id="c38598"]/div/table[1]/tbody')
     rows = table.find_elements(By.TAG_NAME, "tr")
     region = {}
     for row in rows:
@@ -25,7 +25,7 @@ def main(response):
     with open(path, 'w') as outfile:
         json.dump(region, outfile)
 
-    table = driver.find_element_by_xpath('//*[@id="c37759"]/div/table[2]/tbody')
+    table = driver.find_element_by_xpath('//*[@id="c38598"]/div/table[2]/tbody')
     rows = table.find_elements(By.TAG_NAME, "tr")
     
     region = {}
