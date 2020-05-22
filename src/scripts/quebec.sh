14a380df876a9c71cafd0f2fdafac00a998747c4
Nicolas Bent <chocholatethunder@Nicolass-MacBook-Air.local>
2020-05-22 18:24:44 -0400

    Moved files

--- src/scripts/quebec.sh
+++ src/scripts/quebec.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-jupyter nbconvert --to notebook --inplace --execute notebooks/Graphing\ Quebec.ipynb
+cd src/scrapers/quebec/
+scrapy crawl table_quebec
+cd ../../..
+python src/scripts/merge_new_quebec.py 'May 21st'
+python src/scripts/make_graphs_quebec.py
+mv data/quebec/age_cases* data/quebec/past
+mv data/quebec/age_deaths* data/quebec/past
+mv data/quebec/cases* data/quebec/past
+mv data/quebec/deaths* data/quebec/past
+
 
